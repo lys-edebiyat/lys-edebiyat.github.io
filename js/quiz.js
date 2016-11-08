2b33eaf72184cedd052d6c65db3dc2467ef93901
Burak Karakan <burak.karakan@boosmart.com>
2016-11-09 01:47:56 +0200

    Oyun yapısı tamamlandı, çalışıyor.

--- js/quiz.js
+++ js/quiz.js
@@ -2,9 +2,13 @@
 // Parse the JSON dataset first.
 var books = JSON.parse(yazarKitap);
 var count = Object.keys(books).length;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var question;
 
 // Global answer index.
 var answerIndex = GetRandomIndex(3);
+var choices = [3];
 
 // Create the initial question.
 CreateQuestion();
@@ -12,60 +16,92 @@ CreateQuestion();
 // Create a question and pass it to the HTML.
 function CreateQuestion(){
 	var randomIndex = GetRandomIndex(count);
-	var question = books[randomIndex];
-	var choices = [3];
 	var choice;
 
+	question = books[randomIndex];
+	
 	// Do not get long books.
 	while(question.eser.length > 28) {
 		question = books[GetRandomIndex(count)];		
 	}
 
 	// Print the book name to the box.
-	$("#kitap_adi").html(question.eser);
-
-	// Put the real answer into the array.
-	answerIndex = GetRandomIndex(3);
-	choices[answerIndex] = question.yazar;
+	$("#book_name").html(question.eser);
 
 	// Get random author names.
 	for(var i = 0; i < 3; i++) {
 		choice = books[GetRandomIndex(count)];
 
-		if(i !== answerIndex ){
-			if(choices.indexOf(choice.yazar) == -1 || choice.yazar.length > 22){
-				if(question.yazar !== choice.yazar){
-					if((question.hasOwnProperty("digerYazar") && choice.yazar == question.digerYazar) ||
-						(choice.hasOwnProperty("digerYazar") && choice.digerYazar == question.yazar)) {
-						i--;
-					} else {
-						choices[i] = choice.yazar;
-					}
+		if(choices.indexOf(choice.yazar) == -1 || choice.yazar.length > 22){
+			if(question.yazar !== choice.yazar){
+				if(!((question.hasOwnProperty("digerYazar") && choice.yazar == question.digerYazar) ||
+					(choice.hasOwnProperty("digerYazar") && choice.digerYazar == question.yazar))) {
+					
+					// If all the conditions are satisfied, save it as an option.
+					choices[i] = choice.yazar;
+
 				} else { i--; }
 			} else { i--; }
-		}
+		} else { i--; }
 	}
+
+	// Put the real answer into the array.
+	answerIndex = GetRandomIndex(3);
+	choices[answerIndex] = question.yazar;
 
 	// Print the choices to the buttons.
 	for(var i = 0; i < 3; i++) {
-		$('#cevap_' + i).html(choices[i]);
+		$('#button_' + i).html(choices[i]);
 	}
-
-	console.log("answer: " + answerIndex);
 }
 
+// Check the answer and continue to create a new question.
 function CheckAndCreateNewQuestion(answer){
 	if(answerIndex == answer){
-		console.log("doğru cevap.");
-	} else {
-		console.log("yanlış cevap.");
-	}
+		correctAnswers++;
 
-	CreateQuestion();
+		// Create the sweet alert.
+		swal({
+		  title: "Doğru cevap!",
+		  text: "Tebrikler, doğru cevapladın.",
+		  type: "success",
+		  confirmButtonColor: "#f85f73",
+		  confirmButtonText: "Sıradaki Soru",
+		  customClass: "sweetAlertModal"
+		},
+
+		// If the user clicks on the link "Next Question", prepare the question.
+		function(isConfirm){
+		  if (isConfirm) {
+		  	$('#correctAnswers').html(correctAnswers);
+		    CreateQuestion();
+		  }
+		});
+	} else {
+		wrongAnswers++;
+
+		// Create the sweet alert.
+		swal({
+		  title: "Yanlış cevap :(",
+		  text: "<strong>" + question.eser + "</strong> adlı eser <strong>" + question.yazar + "</strong> tarafından yazılmıştır.",
+		  type: "error",
+		  confirmButtonColor: "#636467",
+		  confirmButtonText: "Sıradaki Soru",
+		  customClass: "sweetAlertModal",
+		  html: true
+		},
+
+		// If the user clicks on the link "Next Question", prepare the question.
+		function(isConfirm){
+		  if (isConfirm) {
+			$('#wrongAnswers').html(wrongAnswers);
+		    CreateQuestion();
+		  }
+		});
+	}
 }
 
-
-
-function GetRandomIndex(between){
-	return Math.floor(Math.random() * between);  
+// Generate random number from 0 to end.
+function GetRandomIndex(end){
+	return Math.floor(Math.random() * end);  
 }
